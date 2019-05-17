8916155cb115965b491a3979910958b0c3855af0
michael-whi <michael_tugushev@mail.ru>
2019-05-17 19:21:47 +0300

    UI

--- src/components/App.js
+++ src/components/App.js
@@ -6,61 +6,89 @@ import getRandomData from './../utils/dataGenerator'
 import { Row, Col } from 'antd'
 import 'antd/dist/antd.css'
 
-const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-`;
-
 const Header = styled.section`
     font-size: 45px;
     font-weight: lighter;
     padding-left: 30px;
     border-bottom: 1px solid #e0e0e0;
-
 `;
 
 const LeftPane = styled.section`
     height: calc(100vh - 90px);
-    font-size: 45px;
     font-weight: lighter;
     padding: 20px;
-    box-shadow: 0px 9px 8px #f7f5f5;
+    box-shadow: 0px 9px 21px #dadada
+    background: #fff;
+    height: 280px;
+`;
+
+const Plate = styled.section`
+    padding: 20px;
+    box-shadow: 0px 9px 21px #dadada;
+    width: 50%;
+    margin-top: 20px;
+    margin-left: 15px;
+`;
+
+const Input = styled.input`
+    padding: 0.5em;
+    margin: 0.5em;
+    border-radius: 3px;
+    font-size: 15px;
+    border: 1px solid #ccc;
+    width: 80%;
+`;
+
+const Info = styled.textarea`
+    padding: 0.5em;
+    margin: 0.5em;
+    border-radius: 17px;
+    font-size: 15px;
+    border: 1px solid #f4f4f4;
+    width: 80%;
+    height: 160px;
+    resize: none;
 `;
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {}
+
+    state = { neighbourAmount: 5 }
+
+    handleNeighbourChange = (event) =>{
+        this.setState({ neighbourAmount: event.target.value })
     }
 
     render() {
         const data = getRandomData()
-
+        const { neighbourAmount } = this.state
         return (
             <Fragment>
-            <Row>
-                <Col span={24}>
-                    <Header>
-                        K nearest neighbour
+                <Row>
+                    <Col span={24}>
+                        <Header>
+                            K nearest neighbour
                     </Header>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={4}>
-                    <LeftPane>
-                        
-                    </LeftPane>    
-                </Col>
-                <Col span={20}>
-                    <Wrapper>
-                        <div className="graph-column">
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={4} style={{ padding: '20px 20px 20px 20px' }}>
+                        <LeftPane>
+                            Amount of neighbours
+                            <Input
+                                value={neighbourAmount}
+                                onChange={this.handleNeighbourChange}
+                            />
+                            <Info placeholder="Make double click to create a new point" />
+                        </LeftPane>
+                    </Col>
+                    <Col span={20}>
+                        <Plate>
                             <Graph
                                 data={data}
                             />
-                        </div>
-                    </Wrapper>
-                </Col>
-            </Row>
+                        </Plate>
+                    </Col>
+                </Row>
             </Fragment>
         );
     }
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -8,11 +8,53 @@ class Graph extends Component {
 
     state = { data: [], lastPoint: null };
 
-    componentDidUpdate(prevProps, prevState) {
-        
+    clearStaleSegments(){
+
     }
 
-    componentDidMount() {
+    onCreateNewPoint() {
+        const { lastPoint, data } = this.state
+        console.log('double')
+        if (this.o) {
+            const newPoint = {
+                "type": "scatter",
+                "mode": "markers",
+                "x": this.o.xvals,
+                "y": this.o.yvals,
+                "marker": {
+                    "color": "#777777",
+                    "size": [10 * this.o.xvals / 2],
+                    "sizeref": 1,
+                    "sizemin": 1
+                },
+                "text": [this.o.xvals, this.o.yvals]
+            }
+            const updatedList = update(this.state.data, { $push: [newPoint] })
+            this.setState({ data: updatedList, lastPoint: newPoint }, () => {
+                const pointIndex = data.findIndex(point => point.x[0] === lastPoint.x[0] && point.y[0] === lastPoint.y[0])
+                const test = data.map((f) => ({ x: f.x, y: f.y }))
+                const neighbourList = getNearestNeighbours(lastPoint.x[0], lastPoint.y[0], 5, test)
+                const lineList = neighbourList.map((n) => (
+                    {
+                        "type": "line",
+                        "x": [n.x, data[pointIndex].x[0]],
+                        "y": [n.y, data[pointIndex].y[0]],
+                        "marker": {
+                            "color": "#1890ff"
+                        },
+                        "text": [n.distance]
+                    }
+                ))
+                //have to clean the lines of previous point and add new lines
+                let updatedData = update(this.state.data, { $push: lineList })
+                
+                updatedData = update(this.state.data, { [pointIndex]: { text: { $set: [data[pointIndex].type] } } })
+                this.setState({ data: updatedData })
+            })
+        }
+    }
+
+    makeDataForGraph(){
         const data = this.props.data.map(f => ({
             "type": "scatter",
             "mode": "markers",
@@ -20,7 +62,7 @@ class Graph extends Component {
             "y": [f.y],
             "marker": {
                 "color": f.fill,
-                "size": [10 * (f.x/2)],
+                "size": [10 * (f.x / 2)],
                 "sizeref": 1,
                 "sizemin": 1
             },
@@ -46,24 +88,14 @@ class Graph extends Component {
         }*/)
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.lastPoint !== this.state.lastPoint){
-            const test = this.props.data.map((f) => ({ x: f.x, y: f.y }))
-            const neighbourList = getNearestNeighbours(this.state.lastPoint.x, this.state.lastPoint.y, 5, test)
-            const lineList = neighbourList.map((n) => (
-                {
-                    "type": "line",
-                    "x": [n.x, point.x],
-                    "y": [n.y, point.y],
-                    "marker": {
-                        "color": "#1890ff"
-                    },
-                    "text": [n.distance]
-                }
-            ))
+    componentDidMount() {
+        this.makeDataForGraph()
+    }
 
-            const updatedData = update(this.state.data, { $push: lineList })
-            this.setState({ data: updatedData })
+    componentDidUpdate(prevProps, prevState) {
+        const { lastPoint, data } = this.state
+        if (prevState.lastPoint !== lastPoint && lastPoint.x) {
+
         }
     }
 
@@ -75,26 +107,7 @@ class Graph extends Component {
                     data={this.state.data}
                     layout={{ width: '100%', height: '100%', title: 'Oranges and grapefruits' }}
                     onHover={(o) => this.o = o}
-                    onDoubleClick={() => {
-                        console.log('double')
-                        if (this.o) {
-                            const newPoint = {
-                                "type": "scatter",
-                                "mode": "markers",
-                                "x": this.o.xvals,
-                                "y": this.o.yvals,
-                                "marker": {
-                                    "color": "#777777",
-                                    "size": [10*this.o.xvals/2],
-                                    "sizeref": 1,
-                                    "sizemin": 1
-                                },
-                                "text": [this.o.xvals, this.o.yvals]
-                            }
-                            const updatedList = update(this.state.data, { $push: [newPoint] })
-                            this.setState({ data: updatedList, lastPoint: newPoint })
-                        }
-                    }}
+                    onDoubleClick={this.onCreateNewPoint.bind(this)}
                 />}
             </Fragment>
         );
